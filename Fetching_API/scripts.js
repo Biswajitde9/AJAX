6765f3186069c69cbeb5b4b3a854981a05c9d5ec
Biswajit De <86996368+Biswajitde9@users.noreply.github.com>
2023-07-14 10:41:09 +0530

    Add files via upload

---
+++ Fetching_API/scripts.js
@@ -0,0 +1,16 @@
+function fetchRandomDogImage(){
+
+    var xhrRequest = new XMLHttpRequest();
+
+    xhrRequest.onload = function(){
+        console.log(xhrRequest.response);
+        var responseJSON = JSON.parse(xhrRequest.response);
+        var imageURL= responseJSON.message;
+        $('#dog-image').attr('src',imageURL);
+    };
+    xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true)
+    xhrRequest.send();
+}
+
+
+$('#fetch-dog-image-button').click(fetchRandomDogImage);
